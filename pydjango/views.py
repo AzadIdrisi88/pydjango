aee911d87b3d7910a87ac944beb51590d1389843
AzadIdrisi88 <127969229+AzadIdrisi88@users.noreply.github.com>
2023-06-12 14:48:36 +0530

    Initial commit

---
+++ pydjango/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, HttpResponse, redirect
+
+
+def star(request):
+    return HttpResponse("Hello")
+
+
+def home(request):
+    n1 = 0
+    n2 = 0
+    result = 0
+    if request.GET:
+        print("Get")
+        n1 = int(request.GET["a"])
+        print(n1)
+        n2 = int(request.GET["b"])
+        print(n2)
+        cmd=request.GET["cmd"]
+        if cmd=="Add":
+            result = n1+n2
+        if cmd=="Sub":
+            result=n1-n2
+        # if cmd=="Mul":
+            # result=n1*n2
+        # if cmd=="Div":
+            # result=n1//n2
+
+    return render(request, "radio.html", {"a": n1, "b": n2, "result": result})
